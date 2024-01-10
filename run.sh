bfabd1c91df8c79557525a4395e6b9b2c28f1c8a
Aesku <ali.esuqembre@gmail.com>
2024-01-10 11:52:30 +0100

    CrI3 QE files

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --job-name=NiBr2nscf
+#SBATCH --nodes=1
+#SBATCH --ntasks=24
+#SBATCH --nodelist=compute02
+#SBATCH --mem=12G
+
+#commands
+module purge
+module load qe/7.0-intelmpi
+
+srun --mpi=pmi2 pw.x -inp nscf-nosoc-lda.in
