06864397d5ff106c65fa7756c238293c1bdc855a
Aesku <ali.esuqembre@gmail.com>
2024-01-19 11:45:37 +0100

    soc benchmarking

--- cri3/benchmarking/soc/kpoints-conv.sh
+++ cri3/benchmarking/soc/kpoints-conv.sh
@@ -5,7 +5,7 @@
 kpoints='6 8 12 16 18'
 #
 for i in ${kpoints} ; do
-    sed  -e "s/8 8/$i $i/g" cri3-exp-soc.scf.in  > tmp$i
+    sed  -e "s/6 6/$i $i/g" cri3-exp-soc.scf.in  > tmp$i
     sed  -e "s/prefix = 'cri3'/prefix = 'cri3_90Ry_k$i$@x$i$@x1'/g"  tmp$i  > 'cri3-exp-soc_90Ry_k'$i'x'$i'x1.scf.in'
     rm tmp*
 done
--- cri3/benchmarking/soc/run.sh
+++ cri3/benchmarking/soc/run.sh
@@ -9,4 +9,8 @@
 module purge
 module load qe/7.0-intelmpi
 
-srun --mpi=pmi2 pw.x -inp cri3-exp-soc_90Ry_k6x6x1.scf.in | tee scf-soc_90Ry_k6x6x1.log
+#srun --mpi=pmi2 pw.x -inp cri3-exp-soc_50Ry_k8x8x1.scf.in | tee scf-soc_50Ry_k8x8x1.log
+#srun --mpi=pmi2 pw.x -inp cri3-exp-soc_70Ry_k8x8x1.scf.in | tee scf-soc_70Ry_k8x8x1.log
+#srun --mpi=pmi2 pw.x -inp cri3-exp-soc_90Ry_k8x8x1.scf.in | tee scf-soc_90Ry_k8x8x1.log
+#srun --mpi=pmi2 pw.x -inp cri3-exp-soc_120Ry_k8x8x1.scf.in | tee scf-soc_120Ry_k8x8x1.log
+srun --mpi=pmi2 pw.x -inp cri3-exp-soc_150Ry_k8x8x1.scf.in | tee scf-soc_150Ry_k8x8x1.log
